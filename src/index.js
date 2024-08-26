1ea5c2942f28a5bad34d16dde9dfda7720aaa7a1
emmenove <cesco@emmenove.com>
2024-08-26 17:00:20 +0200

    disabilitati i dev tool

--- src/index.js
+++ src/index.js
@@ -272,7 +272,7 @@ const createWindow = () => {
   Menu.setApplicationMenu(menu);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
 };
 
